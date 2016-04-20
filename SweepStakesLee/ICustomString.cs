54e52ad51a3ae3e1f06ee376a8604af6457c9d93
esysop <leehassel@gmail.com>
2016-04-20 12:14:53 -0500

    Made empty classes and interface

---
+++ SweepStakesLee/ICustomString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakesLee
+	{
+	interface ICustomString
+		{
+		string ToString ();
+
+
+		void Insert (string stringToInsert);
+
+
+
+		void Remove (int startIndex, int numCharsToRemove);
+
+
+
+		int Length ();
+
+
+
+		}
+	}
